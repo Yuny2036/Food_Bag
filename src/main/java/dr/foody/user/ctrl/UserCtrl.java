38f010ada647a21fc363e5edf8140db0bf46e1aa
Yuny2036 <officeryounhee@gmail.com>
2023-11-22 09:19:38 +0900

    Got supported for API implementing.

--- src/main/java/dr/foody/user/ctrl/UserCtrl.java
+++ src/main/java/dr/foody/user/ctrl/UserCtrl.java
@@ -2,6 +2,7 @@ package dr.foody.user.ctrl;
 
 import dr.foody.user.dto.UserDto;
 import dr.foody.user.svc.UserSvc;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -29,5 +30,27 @@ public class UserCtrl {
     @PutMapping("/update")
     public Object modInsDev( @ModelAttribute UserDto userDto) {
         return userService.modify(userDto);
+    }
+
+    @GetMapping("/login")
+    public Object login( @ModelAttribute UserDto userDto) {
+        Integer userIdx = userService.login(userDto);
+        String rst_cd ;
+        if( userIdx > 0)
+            rst_cd = "200";
+        else
+            rst_cd = userIdx.toString();
+
+        JSONObject obj = new JSONObject();
+        obj.put("rst_cd", rst_cd);
+        obj.put("user_idx", userIdx);
+        return obj.toString();
+
+        // API  : /user/login
+        // 호출방식 : Get
+        // Input : email(String), pwd(String)
+        // output : rst_cd : -1 : 아이디 없음,   -2 : pw 틀림, 200 : 로그인 성공
+        //          user_idx : 로그인된 user의 키값. 로그인이 실패라면 쓰레기 값이 input되어있음.
+
     }
 }
--- src/main/java/dr/foody/user/svc/UserSvc.java
+++ src/main/java/dr/foody/user/svc/UserSvc.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Slf4j
 @Service
 public class UserSvc {
@@ -13,9 +15,9 @@ public class UserSvc {
     UserDao userDao;
 
     public Object getList(UserDto userDto){
-        System.out.println(userDao.getList(userDto));
         return userDao.getList(userDto);
     }
+
     public Object modify(UserDto userDto){
         return userDao.mod(userDto);
     }
@@ -24,6 +26,56 @@ public class UserSvc {
     }
     public Object delete(Integer idx){
         return userDao.del(idx);
+    }
+
+    // login
+    public Integer login(UserDto userDto){
+
+        // return 값은 임의로 정의하였으며, 추후 return 값을 규정하여 진행하는것을 추천함
+        // ex )  1 : 성공    1 이외의값은 실패이며 약속한 코드를 리턴함
+        // ID 검사 하는 부분
+        if(!isChkId(userDto.getEmail()))  // id가 없다면 -1 return
+            return -1;
+
+        // pw 겁사하는 부분
+        Integer userIdx = isChkPwd(userDto.getEmail(), userDto.getPwd());
+        if(userIdx < 0) // id와 pw가 일치하는게 없다면 -2 return
+            return -2;
+
+        return userIdx;
+    }
+
+    // ID 를 검증하는 함수
+    private Boolean isChkId(String email)
+    {
+        // email을 아이디로 쓴다고 가정함.
+        UserDto userDto = new UserDto();
+
+        userDto.setEmail(email);
+        // user Table을 Select 함.
+        List<UserDto> userList = userDao.getList(userDto);
+        if(userList.size() == 0)// select한 row가 없다면..
+            return false;  // 검색된 email이 없다면 회원가입이 안되어있다고 판단.
+        else
+            return true;  // 검색된 row가 있다면 아이디 존재
+    }
+
+    // passwd 를 검증 하는 함수
+    private Integer isChkPwd(String email, String pw)
+    {
+        // email을 아이디로 쓴다고 가정함.
+        UserDto userDto = new UserDto();
+
+        userDto.setEmail(email);
+        userDto.setPwd(pw);
+        // user Table을 Select 함.
+        List<UserDto> userList = userDao.getList(userDto);
+        if(userList.size() == 0)  // select한 row가 없다면..
+            return -1;  // email과 pw 모두 맞는게 없다면 -1을 return
+        else
+            return userList.get(0).getIdx();  // 검색된 row가 있다면 아이디 키값을 리턴
+
+
     }
 
 }
